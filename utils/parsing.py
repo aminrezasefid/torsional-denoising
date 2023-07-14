48361709ca58b3f4496d5c80f1f5c6fb2aee450d
Amin <aminreza_sefid@yahoo.com>
2023-07-14 15:27:50 +0330

    batch size updated

--- utils/parsing.py
+++ utils/parsing.py
@@ -16,7 +16,7 @@ def parse_train_args():
 
     # Training arguments
     parser.add_argument('--n_epochs', type=int, default=250, help='Number of epochs for training')
-    parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
+    parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
     parser.add_argument('--lr', type=float, default=1e-3, help='Initial learning rate')
     parser.add_argument('--num_workers', type=int, default=1, help='Number of workers for preprocessing')
     parser.add_argument('--optimizer', type=str, default='adam', help='Adam optimiser only one supported')
